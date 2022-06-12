dd043983d853f87811fda1077d138effa4fda517
Morchid Salaheddine <89920306+SalaheddineS@users.noreply.github.com>
2022-06-12 23:20:08 +0100

    Ajout Registration + Recherche

---
+++ todo_list/main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from .views import TaskList ,TaskDetail,TaskCreate , TaskUpdate  ,DeleteView,CustomLoginView,RegisterPage
+from django.contrib.auth.views import LogoutView
+app_name="main"
+
+urlpatterns = [
+ path('login/',CustomLoginView.as_view(),name='login'),
+ path('logout/',LogoutView.as_view(next_page='main:login'),name='logout'),
+ path('',TaskList.as_view(),name='tasks'),
+ path('register/', RegisterPage.as_view(), name='register'),
+ path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+ path('task-create/', TaskCreate.as_view(), name='task-create'),
+ path('task-create/', TaskCreate.as_view(), name='task-create'),
+ path('task-update/<int:pk>/',TaskUpdate.as_view(), name='task-update'), 
+ path('task-delete/<int:pk>/',DeleteView.as_view(), name='task-delete'),
+
+]
